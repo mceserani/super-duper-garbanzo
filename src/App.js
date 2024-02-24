3d9925481762c5a4ac13ae31c73dcb1099d4f87e
Matteo Ceserani <matteo.ceserani@polomanettiporciatti.edu.it>
2024-02-24 22:27:42 +0100

    Style + Global status

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Square({ value, handleClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="square" onClick={handleClick}>
+      { value }
+    </button>
   );
 }
 
-export default App;
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const newSquares = [...squares];
+    newSquares[i] = 'X';
+    setSquares(newSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} handleClick={() => handleClick(0)}/>
+        <Square value={squares[1]} handleClick={() => handleClick(1)}/>
+        <Square value={squares[2]} handleClick={() => handleClick(2)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} handleClick={() => handleClick(3)}/>
+        <Square value={squares[4]} handleClick={() => handleClick(4)}/>
+        <Square value={squares[5]} handleClick={() => handleClick(5)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} handleClick={() => handleClick(6)}/>
+        <Square value={squares[7]} handleClick={() => handleClick(7)}/>
+        <Square value={squares[8]} handleClick={() => handleClick(8)}/>
+      </div>
+  </>
+  );
+}
